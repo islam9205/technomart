13392aa57c8545568fee11b6ee5dce499b498900
Ислам <islam9205@mail.ru>
2022-07-27 10:44:04 +0300

    1

--- js/map.js
+++ js/map.js
@@ -7,15 +7,13 @@ mapLink.addEventListener("click", function (evt) {
   mapPopup.classList.add("modal-show");
 });
 
-mapClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
+mapClose.addEventListener("click", function () {
   mapPopup.classList.remove("modal-show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (mapPopup.classList.contains("modal-show")) {
-      evt.preventDefault();
       mapPopup.classList.remove("modal-show");
     }
   }
