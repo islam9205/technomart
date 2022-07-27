c09e3104e295334e7d76a468514dec28a82d1b93
Ислам <islam9205@mail.ru>
2022-07-27 16:01:17 +0300

    1

--- js/added-to-cart.js
+++ js/added-to-cart.js
@@ -1,12 +1,22 @@
-const catalogButtonBuy = document.querySelectorAll('.catalog-button-buy');
+const catalogButtonBuy = document.querySelector('.catalog-button-buy');
 const addedToCart = document.querySelector('.added-to-cart');
 const closeButton = addedToCart.querySelector('.popup-button');
 const AddedToCartLink = addedToCart.querySelector('.added-to-cart__link')
 
-for (let i=0; i<catalogButtonBuy.length; i++) {
-  catalogButtonBuy.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    writeToUsPopap.classList.add("modal-show");
-    nameInput.focus();
-  });
-}
+catalogButtonBuy.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  addedToCart.classList.add("modal-show");
+  AddedToCartLink.focus();
+});
+
+/* for (let i=0; i<catalogButtonBuy.length; i++) {
+
+} */
+/*
+catalogButtonBuy.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeToUsPopap.classList.add("modal-show");
+  AddedToCartLink.focus();
+});
+
+ */
